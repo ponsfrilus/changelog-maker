25547a4e749d452667cc4891d6fede9f40696654
Nicolas Borboën <ponsfrilus@gmail.com>
2021-08-03 14:27:21 +0200

    [fix] Clean up tags

--- changelog.js
+++ changelog.js
@@ -158,7 +158,7 @@ async function sortCommits() {
   const beforeField = document.getElementById("beforedate").value.toString();
   const afterField = document.getElementById("afterdate").value.toString();
   const rawCommits = await getCommits("https://api.github.com/repos/" + urlField + "/commits?per_page=", "100", apiField, beforeField, afterField);
-  const commitMessages = rawCommits.map((item) => "[[" + item.sha.substring(0, 8) + "](" + item.html_url + ")] -" + item.commit.message.split("\n")[0]);
+  const commitMessages = rawCommits.map((item) => "[[" + item.sha.substring(0, 8) + "](" + item.html_url + ")] - " + item.commit.message.split("\n")[0]);
   const features = [];
   const fixes = [];
   const refs = [];
@@ -167,7 +167,8 @@ async function sortCommits() {
   commitMessages.forEach(function callbackFn(commit) { 
     featureKwList.forEach(function callbackFn(balise) {
       if (commit.match(new RegExp(`(?!\\)\\] - )\\[?${balise}[\\]|:]`, "g"))) {
-        features.push(commit.replace(new RegExp(`(?!\\)\\] - )\\[?${balise}[\\]|:]`, "g"),''));
+        //features.push(commit.replace(new RegExp(`(?!\\)\\] - )\\[?${balise}[\\]|:]`, "g"),''));
+        features.push(commit);
       } 
     });
   });
@@ -175,7 +176,8 @@ async function sortCommits() {
   commitMessages.forEach(function callbackFn(commit) { 
     fixesKwList.forEach(function callbackFn(balise) {
       if (commit.match(new RegExp(`(?!\\)\\] - )\\[?${balise}[\\]|:]`, "g"))) {
-        fixes.push(commit.replace(new RegExp(`(?!\\)\\] - )\\[?${balise}[\\]|:]`, "g"),''));
+        // fixes.push(commit.replace(new RegExp(`(?!\\)\\] - )\\[?${balise}[\\]|:]`, "g"),''));
+        fixes.push(commit);
       } 
     });
   });
@@ -183,7 +185,8 @@ async function sortCommits() {
   commitMessages.forEach(function callbackFn(commit) { 
     refactorKwList.forEach(function callbackFn(balise) {
       if (commit.match(new RegExp(`(?!\\)\\] - )\\[?${balise}[\\]|:]`, "g"))) {
-        refs.push(commit.replace(new RegExp(`(?!\\)\\] - )\\[?${balise}[\\]|:]`, "g"), ''));
+        // refs.push(commit.replace(new RegExp(`(?!\\)\\] - )\\[?${balise}[\\]|:]`, "g"), ''));
+        refs.push(commit);
       } 
     });
   });
